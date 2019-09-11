b4640c1c1a68ca2f63aeb2c2aff3d7ee29974c0a
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-11 21:26:05 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,39 @@
-# Add your code here
+require 'pry'
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  songs.each {|song| puts "#{songs.rindex(song)+1}. #{song}"}
+end
+
+def play(songs)
+  puts "Please enter a song name or number: "  
+  song_request = gets.strip
+  
+end
+
+
+# def list(songs)
+#   songs.each_with_index do |value, index|
+#     puts "#{index + 1}. #{value}"
+#   #binding.pry
+# end
+# end
