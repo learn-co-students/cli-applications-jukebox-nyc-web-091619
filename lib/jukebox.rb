3e791f3cd5550300b6388efaec237ac1d1b66aff
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-12 05:06:32 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,30 +27,36 @@ end
 def play(songs)
   puts "Please enter a song name or number: "  
   song_request = gets.strip
-  binding.pry
-  case song_request
-  when song_request == ("1" || "Phoenix - 1901")
-    puts "#{songs[0]}"
-    binding.pry
-  when song_request == ("2" || "Tokyo Police Club - Wait Up")
-    puts "#{songs[1]}"
-  when song_request == ("3" || "Sufjan Stevens - Too Much")
-    puts "#{songs[2]}"
-  when song_request == ("4" || "The Naked and the Famous - Young Blood")
-    puts "#{songs[3]}"
-  when song_request == ("5" || "(Far From) Home - Tiga")
-    puts "#{songs[4]}"
-  when song_request == ("6" || "The Cults - Abducted")
-    puts "#{songs[5]}"
-  when song_request == ("7" || "Phoenix - Consolation Prizes")
-    puts "#{songs[6]}"
-  when song_request == ("8" || "Harry Chapin - Cats in the Cradle")
-    puts "#{songs[7]}"
-  when song_request == ("9" || "Amos Lee - Keep It Loose, Keep It Tight")
-    puts "#{songs[8]}
+  if song_request.to_i.between?(1..songs.length)
+    puts "Playing #{songs[song_request.to_i]}"
+  elsif songs.include?(song_request)
+    puts "Playing #{songs.find{|s| s == song_request}}"
+  else 
+    puts "Invalid input, please try again"
   end
 end
 
+
+# case song_request
+#   when song_request == ("1" || "Phoenix - 1901")
+#     puts "#{songs[0]}"
+#     binding.pry
+#   when song_request == ("2" || "Tokyo Police Club - Wait Up")
+#     puts "#{songs[1]}"
+#   when song_request == ("3" || "Sufjan Stevens - Too Much")
+#     puts "#{songs[2]}"
+#   when song_request == ("4" || "The Naked and the Famous - Young Blood")
+#     puts "#{songs[3]}"
+#   when song_request == ("5" || "(Far From) Home - Tiga")
+#     puts "#{songs[4]}"
+#   when song_request == ("6" || "The Cults - Abducted")
+#     puts "#{songs[5]}"
+#   when song_request == ("7" || "Phoenix - Consolation Prizes")
+#     puts "#{songs[6]}"
+#   when song_request == ("8" || "Harry Chapin - Cats in the Cradle")
+#     puts "#{songs[7]}"
+#   when song_request == ("9" || "Amos Lee - Keep It Loose, Keep It Tight")
+#     puts "#{songs[8]}"
 
 # def list(songs)
 #   songs.each_with_index do |value, index|
